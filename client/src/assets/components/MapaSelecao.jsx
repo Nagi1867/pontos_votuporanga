0d7f712a8a55ed8be91ecf6da2b70a8c334ebb78
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2026-06-15 20:56:27 -0300

    detalhes visuais corrigidos

--- client/src/assets/components/MapaSelecao.jsx
+++ client/src/assets/components/MapaSelecao.jsx
@@ -1,12 +1,46 @@
-import { MapContainer, TileLayer, Marker, useMapEvents } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  useMapEvents,
+  useMap,
+} from "react-leaflet";
+import { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
+// Corrige o problema do ícone não aparecer no React
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",
+  iconUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  shadowUrl:
+    "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png",
+});
+
+// Componente que move o mapa até o ponto selecionado
+function CentralizarMapa({ latitude, longitude }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (latitude && longitude) {
+      map.flyTo([latitude, longitude], 16);
+    }
+  }, [latitude, longitude, map]);
+
+  return null;
+}
+
+// Componente responsável por capturar o clique e exibir o marcador
 function LocalizacaoMarker({
   latitude,
   longitude,
   setLatitude,
   setLongitude,
 }) {
-
   useMapEvents({
     click(e) {
       setLatitude(e.latlng.lat);
@@ -19,7 +53,9 @@ function LocalizacaoMarker({
   }
 
   return (
-    <Marker position={[latitude, longitude]} />
+    <Marker
+      position={[latitude, longitude]}
+    />
   );
 }
 
@@ -29,12 +65,10 @@ export default function MapaSelecao({
   setLatitude,
   setLongitude,
 }) {
-
-  const centroInicial = [-20.4215, -49.9728]; // região de Valentim Gentil
+  const centroInicial = [-20.4215, -49.9728];
 
   return (
     <div className="rounded-xl overflow-hidden border">
-
       <MapContainer
         center={centroInicial}
         zoom={13}
@@ -43,10 +77,14 @@ export default function MapaSelecao({
           width: "100%",
         }}
       >
-
         <TileLayer
-          attribution='&copy; OpenStreetMap'
+          attribution="© OpenStreetMap"
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        <CentralizarMapa
+          latitude={latitude}
+          longitude={longitude}
         />
 
         <LocalizacaoMarker
@@ -55,9 +93,7 @@ export default function MapaSelecao({
           setLatitude={setLatitude}
           setLongitude={setLongitude}
         />
-
       </MapContainer>
-
     </div>
   );
 }
