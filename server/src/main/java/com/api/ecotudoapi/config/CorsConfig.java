84451870b529e057c673735c50017505439d3d25
Gabriel Kiyoto <pantanogabriel@gmail.com>
2026-08-26 21:33:02 -0300

    correcoes

--- server/src/main/java/com/api/ecotudoapi/config/CorsConfig.java
+++ server/src/main/java/com/api/ecotudoapi/config/CorsConfig.java
@@ -1,4 +1,5 @@
 package com.api.ecotudoapi.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,18 +8,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
-    public String prod = "https://ecoponto.fitneer.com.br/";
-    public String dev = "http://localhost:3000";
-
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-
                 registry.addMapping("/**")
-                        .allowedOrigins(prod)
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedOrigins(
+                                "https://ecoponto.fitneer.com.br",
+                                "http://localhost:3000"
+                        )
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        )
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
